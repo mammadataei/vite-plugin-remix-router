5d75e0dcd152d8b48781745cc27ada71ae363163
Mohammad Ataei <mammadataei@gmail.com>
2022-09-11 18:23:12 +0430

    chore: add directory meta data to RouteNodes

--- src/buildRouteTree.ts
+++ src/buildRouteTree.ts
@@ -10,6 +10,10 @@ export class RouteNode {
 
   children: Array<RouteNode> = []
 
+  isDirectory?: boolean
+
+  layoutPath?: string
+
   constructor(filePath: string) {
     this.name = path.parse(filePath).name
     this.path = filePath
@@ -17,41 +21,55 @@ export class RouteNode {
 }
 
 export function buildRouteTree(): RouteNode {
-  function createNode(filePath: string) {
-    const node = new RouteNode(filePath)
-
-    if (isDirectory(absolutePath(filePath))) {
-      const children = resolveChildren(filePath)
-
-      node.children = children.map((child) =>
-        createNode(`${filePath}/${child}`),
-      )
-    }
-
-    return node
-  }
-
-  function absolutePath(filePath: string) {
-    return path.join(getOptions().root, filePath)
-  }
-
-  function resolveChildren(directoryPath: string) {
-    const children = fs.readdirSync(absolutePath(directoryPath))
-
-    return children.filter((child) => {
-      const childPath = path.join(directoryPath, child)
-
-      if (isDirectory(childPath)) {
-        return true
-      }
-
-      const extension = path.extname(childPath)
-      return getOptions().extensions.includes(extension.substring(1))
-    })
-  }
-
   const root = createNode(getOptions().pageDir)
   root.name = '/'
 
   return root
+}
+
+function createNode(filePath: string) {
+  const node = new RouteNode(filePath)
+
+  if (isDirectory(absolutePath(filePath))) {
+    node.isDirectory = true
+    node.layoutPath = getLayoutPath(filePath)
+
+    const children = resolveChildren(absolutePath(filePath))
+    node.children = children.map((child) => createNode(`${filePath}/${child}`))
+  }
+
+  return node
+}
+
+function absolutePath(filePath: string) {
+  return path.join(getOptions().root, filePath)
+}
+
+function getLayoutPath(directoryPath: string) {
+  return getOptions()
+    .extensions.map((extension) => `${directoryPath}.${extension}`)
+    .find((filePath) => fs.existsSync(absolutePath(filePath)))
+}
+
+function resolveChildren(directoryPath: string) {
+  const children = fs.readdirSync(directoryPath)
+
+  return children.filter((child) => {
+    const childPath = path.join(directoryPath, child)
+
+    if (isDirectory(childPath)) {
+      return true
+    }
+
+    if (isLayout(childPath)) {
+      return false
+    }
+
+    const extension = path.extname(childPath)
+    return getOptions().extensions.includes(extension.substring(1))
+  })
+}
+
+function isLayout(filePath: string) {
+  return fs.existsSync(filePath.split('.').slice(0, -1).join('.'))
 }
