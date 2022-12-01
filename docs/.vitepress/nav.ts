265c677c6212f78fa1bfbdefdb98c49c73a4360f
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2022-12-01 12:20:31 +0330

    docs: add getting-started guides (#17)

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -2,7 +2,7 @@ import type { DefaultTheme } from 'vitepress'
 
 export function nav(): DefaultTheme.NavItem[] {
   return [
-    { text: 'Guides', link: '/introduction' },
+    { text: 'Guides', link: '/guides/introduction' },
     { text: 'Changelog', link: '#' },
   ]
 }
