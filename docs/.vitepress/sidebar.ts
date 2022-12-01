016d83ebe918c88feef5aa34aa4586e2466eb179
Mohammad Ataei <mammadataei@gmail.com>
2022-12-01 12:16:38 +0330

    docs: add getting-started guides

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -5,9 +5,14 @@ export function sidebar(): DefaultTheme.Sidebar {
     {
       text: 'Guides',
       items: [
-        { text: 'Introduction', link: '/introduction' },
-        { text: 'Getting Started', link: '/getting-started' },
+        { text: 'Introduction', link: '/guides/introduction' },
+        { text: 'Getting Started', link: '/guides/getting-started' },
+        { text: 'Defining Routes', link: '/guides/defining-routes' },
       ],
+    },
+    {
+      text: 'Configuration',
+      items: [{ text: 'Config Reference', link: '/config/reference' }],
     },
   ]
 }
